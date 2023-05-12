343affde6136e0ba1cf0c396f6c30da53bb53476
Денис Щербатюк <denvol100@gmail.com>
2023-05-12 10:47:17 +0300

    Rename project files after clone from github

--- StudyRoomPlagin/Infrastructure/RevitCommand.cs
+++ StudyRoomPlagin/Infrastructure/RevitCommand.cs
@@ -9,9 +9,9 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using Autodesk.Revit.DB.Architecture;
-using RevitWPFTemplate.ViewModels;
+using GettingSimpleInfo.ViewModels;
 
-namespace RevitWPFTemplate.Infrastructure
+namespace GettingSimpleInfo.Infrastructure
 {
     [Transaction(TransactionMode.Manual)]
     [Regeneration(RegenerationOption.Manual)]
